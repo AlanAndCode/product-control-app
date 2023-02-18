f20935227fa4fcb683b24c16bd5600099127ea88
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-02-18 11:17:52 -0300

    feat: add a logo

--- app/src/main/java/com/example/controledeprodutos/Auth/ActivityLogin.java
+++ app/src/main/java/com/example/controledeprodutos/Auth/ActivityLogin.java
@@ -3,11 +3,22 @@ package com.example.controledeprodutos.Auth;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.controledeprodutos.R;
+import com.example.controledeprodutos.activity.MainActivity;
+import com.example.controledeprodutos.helper.FirebaseHelper;
 
 public class ActivityLogin extends AppCompatActivity {
     private TextView text_CC;
+
+    private EditText edit_email;
+    private EditText edit_senha;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +30,54 @@ public class ActivityLogin extends AppCompatActivity {
     configCliques();
 }
 
+public void logar(View view){
+        String email = edit_email.getText().toString().trim();
+        String senha = edit_senha.getText().toString();
+
+        if(!email.isEmpty()){
+if(!senha.isEmpty()){
+
+    progressBar.setVisibility(View.VISIBLE);
+
+    validaLogin(email, senha);
+
+}else {
+edit_senha.requestFocus();
+        edit_senha.setError("Write your password");
+}
+        }else {
+            edit_email.requestFocus();
+            edit_email.setError("Write your email");
+        }
+
+}
+
+private void validaLogin(String email, String senha){
+    FirebaseHelper.getAuth().signInWithEmailAndPassword(
+
+email, senha
+
+    ).addOnCompleteListener(task -> {
+       if(task.isSuccessful()){
+finish();
+startActivity(new Intent(this, MainActivity.class));
+       }else{
+           String error = task.getException().getMessage();
+           Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+           progressBar.setVisibility(View.GONE);
+       }
+    });
+}
     private void configCliques(){
         text_CC.setOnClickListener(view -> startActivity(new Intent(this, CriarContaActivity.class)));
     }
 
     private void  iniciaComponentes(){
         text_CC = findViewById(R.id.text_CC);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+
+        progressBar = findViewById(R.id.progressBar);
     }
 
 }
