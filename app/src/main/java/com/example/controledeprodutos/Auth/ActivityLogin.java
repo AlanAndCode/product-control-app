db2191b87d10976d2984bea37e8b621cd55a86e0
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-02-24 22:25:22 -0300

    feat: adding screen from forgot password and switching databases from SQLite to Realtime Database

--- app/src/main/java/com/example/controledeprodutos/Auth/ActivityLogin.java
+++ app/src/main/java/com/example/controledeprodutos/Auth/ActivityLogin.java
@@ -19,6 +19,8 @@ public class ActivityLogin extends AppCompatActivity {
     private EditText edit_email;
     private EditText edit_senha;
     private ProgressBar progressBar;
+
+    private TextView text_CP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,13 +72,14 @@ startActivity(new Intent(this, MainActivity.class));
 }
     private void configCliques(){
         text_CC.setOnClickListener(view -> startActivity(new Intent(this, CriarContaActivity.class)));
+        text_CP.setOnClickListener(view -> startActivity(new Intent(this, RecuperarContaActivity.class)));
     }
 
     private void  iniciaComponentes(){
         text_CC = findViewById(R.id.text_CC);
         edit_email = findViewById(R.id.edit_email);
         edit_senha = findViewById(R.id.edit_senha);
-
+        text_CP = findViewById(R.id.text_CP);
         progressBar = findViewById(R.id.progressBar);
     }
 
