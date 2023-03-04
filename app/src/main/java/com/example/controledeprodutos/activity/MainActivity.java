76752d2007d7e770804ad5ddf4f5243dce819247
Alan Anderson <64568493+AlanAndCode@users.noreply.github.com>
2023-03-04 15:57:08 -0300

    feat: add a update for RecuperaProdutos

--- app/src/main/java/com/example/controledeprodutos/activity/MainActivity.java
+++ app/src/main/java/com/example/controledeprodutos/activity/MainActivity.java
@@ -1,27 +1,33 @@
 package com.example.controledeprodutos.activity;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.controledeprodutos.Auth.ActivityLogin;
-import com.example.controledeprodutos.activity.FormProdutoActivity;
 import com.example.controledeprodutos.adapter.AdapterProduto;
-import com.example.controledeprodutos.ProductDAO;
 import com.example.controledeprodutos.helper.FirebaseHelper;
 import com.example.controledeprodutos.model.Produto;
 import com.example.controledeprodutos.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.tsuryo.swipeablerv.SwipeLeftRightCallback;
 import com.tsuryo.swipeablerv.SwipeableRecyclerView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterProduto.OnClick {
@@ -31,8 +37,10 @@ public class MainActivity extends AppCompatActivity implements AdapterProduto.On
 private TextView text_info;
     private ImageButton image_add;
     private ImageButton image_info;
+
+    private ProgressBar progressBar;
 private List<Produto> produtoList = new ArrayList<>();
-    private ProductDAO productDAO;
+
 
 
     @Override
@@ -40,13 +48,12 @@ private List<Produto> produtoList = new ArrayList<>();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        productDAO = new ProductDAO(this);
 
-produtoList = productDAO.getListProdutos();
 
     image_add = findViewById(R.id.image_add);
         image_info= findViewById(R.id.image_info);
    text_info = findViewById(R.id.text_info);
+   progressBar = findViewById(R.id.progressBar);
 
         RecyclerProdutos = findViewById(R.id.RecyclerProdutos);
 
@@ -54,15 +61,41 @@ produtoList = productDAO.getListProdutos();
 
         ouvinteCliques();
     }
-
     @Override
     protected void onStart() {
         super.onStart();
+        recuperarProdutos();
 
-        configRecyclerView();
 
     }
+    private void recuperarProdutos(){
+        DatabaseReference produtosRef = FirebaseHelper.getDatabaseReference()
+        .child("produtos")
+                .child(FirebaseHelper.getIdFirebase());
+        produtosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                produtoList.clear();
+                for (DataSnapshot snap : snapshot.getChildren()){
+                    Produto produto = snap.getValue(Produto.class);
 
+
+                    produtoList.add(produto);
+
+                }
+
+                verificaQtdLista();
+
+                Collections.reverse(produtoList);
+                adapterProduto.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
     private void ouvinteCliques(){
         image_add.setOnClickListener(view -> {
             startActivity(new Intent(this, FormProdutoActivity.class));
@@ -87,11 +120,6 @@ popupMenu.show();
 
     private void configRecyclerView(){
 
-        produtoList.clear();
-        produtoList = productDAO.getListProdutos();
-
-        verificaQtdLista();
-
         RecyclerProdutos.setLayoutManager(new LinearLayoutManager(this));
         RecyclerProdutos.setHasFixedSize(true);
         adapterProduto = new AdapterProduto(produtoList, this);
@@ -100,7 +128,6 @@ popupMenu.show();
         RecyclerProdutos.setListener(new SwipeLeftRightCallback.Listener() {
             @Override
             public void onSwipedLeft(int position) {
-
             }
 
             @Override
@@ -109,8 +136,9 @@ popupMenu.show();
                Produto produto = produtoList.get(position);
 
 
-                productDAO.deleteProduct(produto);
+
                 produtoList.remove(produto);
+                produto.deleteProduct();
 adapterProduto.notifyItemRemoved(position);
 
                 verificaQtdLista();
@@ -123,10 +151,13 @@ adapterProduto.notifyItemRemoved(position);
 
 private void verificaQtdLista(){
     if(produtoList.size() == 0){
+        text_info.setText("Nenhum produto cadastrado.");
         text_info.setVisibility(View.VISIBLE);
+
     }else{
         text_info.setVisibility(View.GONE);
     }
+    progressBar.setVisibility(View.GONE);
 }
 
     @Override
